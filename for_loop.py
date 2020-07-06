24e6eba279e84f5296f2f90b0a230c2c62f23099
dogancantorun8 <48547417+dogancantorun8@users.noreply.github.com>
2020-07-06 20:06:54 +0300

    adding continue and break

--- for_loop.py
+++ for_loop.py
@@ -6,7 +6,7 @@ Created on Mon Jun 29 18:04:22 2020
 """
 
 
-#Subject :For loop definition and using objects 
+#Subject :For loop definition and using objects an break continue 
 
 my_list = [10, 20, 30, 40, 50]
 
@@ -24,6 +24,7 @@ for i in r:
 #Get dict's  keys 
 d = {100: 'lebron', 200: 'osman',300:'james',400:'marienne'}
 count=1
+print("\n")
 for i in d:
     print("Printing keys {} ={} " .format(count,i))   
     count +=1 
@@ -70,4 +71,99 @@ for i in s:
 
 #give a direct list    
 for i in (1, 3, 5, 7, 9):
-        print(i**3,end=' ') 
+        print(i**3,end=' ')   
+
+#Create tuple ,tuple has two members
+for i in range(10):
+    for k in range(10):
+        print('({}, {})'.format(i, k), end = ' ')  
+
+#key-value swapping operations
+d = {'dogan': 120, 'atiba': 200, 'elneny': 300, 'vida': 40, 'diaby': 50}        
+k = {}
+
+for key, value in d.items():
+    k[value] = key
+    
+print(k)  
+
+#Odd even list operations
+a = [3, 6, 7, 8, 34, 56, 57, 99, 34]
+even = []
+odd = []
+
+for i in a:
+    if i % 2 == 0:
+        even.append(i)
+    else:
+        odd.append(i)
+        
+print(even)
+print(odd) 
+
+#Standart Deviation  
+a = [3, 6, 7, 8, 34, 56, 57, 99, 34]
+mean = sum(a) / len(a)
+
+total = 0
+for x in a:
+    total += (mean - x) ** 2
+    
+stdev = (total / len(a)) ** 0.5
+print(stdev)  
+
+#String iteration example 
+n = int(input("Bir sayi girin="))
+for s in str(n):                  
+     print(s, end=' ') 
+
+#break example =>if list has "0" loop break and end loop
+a = [10, 4, 5, 0, 7, 9, 8]
+
+for x in a:
+    if x == 0:
+        break
+    print(x)
+    
+print('ends...')   
+
+#find list members and breaking loops 
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]    
+
+val = int(input('Can you find value in list=:'))
+for x in a:
+    if val == x:
+        print('found')
+        break
+else:
+    print('not found!')   
+
+#continue expression only write odd number    
+for i in range(10):
+    if i % 2 == 0:
+        continue
+    print(i)
+
+print('ends...') 
+
+#Create cmd console using break and continue with while loop 
+while True: 
+    print('CSD>', end='')
+    cmd = input().strip()
+    if cmd == '':
+        continue
+    if cmd == 'quit':
+        break
+    if cmd == 'dir':
+        print('dir command')
+    elif cmd == 'copy':
+        print('copy command')
+    elif cmd == 'remove':
+        print('remove command')
+    else:
+        print('bad command!')
+
+
+        
+
+
